13625ed2edd6d45e9212b92bbd978cedbcd440fb
750584 <srr66166@gmail.com>
2025-01-21 20:48:20 +0530

    added on buttonclick() function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'passwordGenerator';
+
+  generatedPassword = '';
+
+  onButtonClick(){
+    this.generatedPassword="generated password";
+  }
+  
 }
